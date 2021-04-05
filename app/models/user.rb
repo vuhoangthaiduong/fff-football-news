6b270c6a61a727e07915029bc4fc6d32d0b9bc85
= <=>
2021-04-05 19:44:23 +0700

    Added sign-up function

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   has_many :articles, dependent: :destroy
   has_many :comments, dependent: :destroy
 
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :username, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                                     format: { with: VALID_EMAIL_REGEX },
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root   'static_pages#home'
   get    '/about',  to: 'static_pages#about'
   get    '/contact',  to: 'static_pages#contact'
+  get    '/signup', to: 'users#new'
 
   resources :news_articles, path: 'news'
+  resources :users
 
 end
