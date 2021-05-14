42e2d23dc5e0d97c5910ce4523cc7419fbcf9c9a
= <=>
2021-05-15 00:16:39 +0700

    Added master data

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,41 @@
 #               password_confirmation: password)
 # end
 
-User.all.each do |user|
-  user.profile_picture.attach(io: File.open("app/assets/images/admin/profile_male.png"), filename: "avatar.jpg", content_type: "image/jpg")
-end
+# User.all.each do |user|
+#   user.profile_picture.attach(io: File.open("app/assets/images/admin/profile_male.png"), filename: "avatar.jpg", content_type: "image/jpg")
+# end
+
+# Position.create(
+#   [
+#     {name: "Quarterback"},
+#     {name: "Running Back"},
+#     {name: "Fullback"},
+#     {name: "Offensive Line"},
+#     {name: "Wide Receivers"},
+#     {name: "Tight End"},
+#     {name: "Defensive Line"},
+#     {name: "Linebacker"},
+#     {name: "Cornerback"},
+#     {name: "Safety"},
+#     {name: "Kicker"},
+#     {name: "Punter"},
+#     {name: "Return Specialists"},
+#     {name: "Long Snapper"}
+#   ]
+# )
+
+Nationality.create(
+  [
+    {name: "Nepal"},
+    {name: "Albania"},
+    {name: "Lithuania"},
+    {name: "Madagascar"},
+    {name: "Canada"},
+    {name: "Denmark"},
+    {name: "Finland"},
+    {name: "Guatemala"},
+    {name: "Honduras"},
+    {name: "Ireland"},
+    {name: "Kuwait"}
+  ]
+)
